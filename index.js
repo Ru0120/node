a38c04c14e04052d29a645d82fff0af5bfa30240
Ru0120 <bavuumsk16@gmail.com>
2025-01-09 20:32:34 +0800

    updated node

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 import http from "http";
+import fs from "fs";
 
 const server = http.createServer((req, res) => {
+  if (req.url === "/news/trend") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    const news = fs.readFileSync("news.json", "utf8");
+
+    console.log(news);
+
+    res.write(JSON.stringify(news));
+    res.end();
+  }
+
   if (req.url === "/zurhai") {
     res.writeHead(200, { "Content-Type": "application/json" });
 
     const zurhai = { udur: "2025.01.09/Purev garag" };
 
     res.write(JSON.stringify(zurhai));
-
     res.end();
   }
 
@@ -15,11 +25,10 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { "Counter-Type": "application/json" });
     const weather = { udur: -15, shunu: -21 };
     res.write(JSON.stringify(weather));
-
     res.end();
   }
-  res.writeHead(404, "not found");
 
+  res.writeHead(404, "not found");
   res.end();
 });
 
